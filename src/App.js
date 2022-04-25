d276c6373948674f1b63b6a744320ba4e091f746
donghyunKang-dev <make_w@naver.com>
2022-04-25 16:06:36 +0900

    [강동현] Set: redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Routes } from 'react-router-dom';
 
 function App() {
   return (
     <React.Fragment>
-      hello wrold
     </React.Fragment>
   );
 };
 
 export default App;
+
+
+    
