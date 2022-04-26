4d8ca4706bab598bad6eef937395d5e8bd6c64e9
donghyunKang-dev <make_w@naver.com>
2022-04-26 17:26:11 +0900

    [강동현] Create:  detail page

---
+++ src/pages/Detail.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import slider from 'react-slick/lib/slider';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"
+import styled from 'styled-components';
+import DetailBottom from '../components/detail/DetailBottom';
+import DetailContent from '../components/detail/DetailContent';
+import SellerBar from '../components/detail/SellerBar';
+
+import { Image, Grid } from '../elements/index';
+import { response } from '../shared/mock';
+
+const Detail = (props) => {
+    const settings = { 
+        infinite: false,
+        dots: true, 
+        speed: 500,
+        slidesToShow: 1, 
+        slidesToScroll: 1,
+    };
+
+    return (
+        <Container>
+            <Grid>
+                <StyledSlider {...settings}>
+                    {response.list.map((v ,i) => {
+                        return(
+                            <Image 
+                                key={v.productId}
+                                src={v.imageUrl}
+                                shape="rectangle"
+                                height="400px"
+                            />
+                        )}
+                    )}
+                </StyledSlider>
+                <SellerBar />
+                <DetailContent />
+            </Grid>
+            <DetailBottom />
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    overflow: hidden;
+`;
+
+const StyledSlider = styled(slider)`
+    .slick-list {  
+        width: 100%;
+        height: 100%;
+        overflow-x: hidden;
+    }
+
+    .slick-dots { 
+        bottom: 20px;
+    }
+`;
+
+export default Detail;
---
+++ src/shared/mock.js
@@ -0,0 +1,29 @@
+export const response = {
+    list: [
+        {
+            imageUrl: "https://images.unsplash.com/photo-1502920917128-1aa500764cbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FtZXJhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            status: "교환대기",
+            productId: 1,
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+            status: "교환대기",
+            productId: 2,
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1518977676601-b53f82aba655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG90YXRvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            status: "교환중",
+            productId: 3,
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1614715838608-dd527c46231d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Z2xhc3Nlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+            status: "교환대기",
+            productId: 4,
+        },
+        {
+            imageUrl: "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            status: "교환대기",
+            productId: 5,
+        }
+    ]
+}
