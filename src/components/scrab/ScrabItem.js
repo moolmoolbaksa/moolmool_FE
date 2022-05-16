791713b380dee5ba2a8053cb8adf424a4d0d5483
donghyunKang-dev <make_w@naver.com>
2022-05-17 02:55:00 +0900

    [강동현] Feat: 찜목록 삭제

--- src/components/scrab/ScrabItem.js
+++ src/components/scrab/ScrabItem.js
@@ -1,38 +1,54 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import { Grid, Text } from '../../elements/index';
+import { Text } from '../../elements/index';
 import { useDispatch } from 'react-redux';
 import { api as productActions } from '../../redux/modules/product';
 
 const ScrabItem = ({itemId, title, contents, image}) => {
     const dispatch = useDispatch();
+    const [look, setLook] = useState(true);
 
     const onGoDetail = () => {
         dispatch(productActions.getProductApi(itemId));
     };
     
+    const onDelete = () => {
+        dispatch(productActions.setProductScrabApi(itemId));
+        setLook(!look)
+    };
+
+    if(!look){
+        return null
+    };
+
     return (
         <Container>
             <Image 
-                onClick={onGoDetail}
                 src={image}
+                onClick={onGoDetail}
             />
-            <Wrap>
-                <Text
-                    onClick={onGoDetail}
-                    text={title}
-                    size="16px"
-                    bold="bold"
-                />
-                <Text
-                    multi="2"
-                    size="12px"
-                    text={contents}
-                    color="#9D9D9D"
-                />
-            </Wrap>
+            <Content>
+                <Wrap onClick={onGoDetail}>
+                    <Text
+                        text={title}
+                        size="16px"
+                        bold="bold"
+                    />
+                    <Text
+                        multi="2"
+                        size="12px"
+                        text={contents}
+                        color="#9D9D9D"
+                    />
+                </Wrap>
+                <span 
+                    className="material-symbols-outlined"
+                    onClick={onDelete}
+                >
+                    close
+                </span>
+            </Content>
         </Container>
     );
 };
@@ -45,6 +61,23 @@ const Container = styled.div`
     border-radius: 10px;
     background-color: white;
     box-shadow: 1px 2px 5px 0px rgba(0, 0, 0, 0.2);
+`;
+
+const Content = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    & span {
+        font-size: 20px;
+        color: lightgray;
+        margin-right: 10px;
+        text-indent: -9999;
+        cursor: pointer;
+
+        &:hover {
+            color: ${props => props.theme.palette.yellow};
+        }
+    };
 `;
 
 const Wrap = styled.div`
