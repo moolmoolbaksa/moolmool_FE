ccc0ea67cbfd802fec452c782156dcf6c38ee62c
donghyunKang-dev <make_w@naver.com>
2022-05-20 17:39:03 +0900

    [강동현] Feat: 알림 채팅 연결

--- src/components/search/SearchItem.js
+++ src/components/search/SearchItem.js
@@ -7,11 +7,18 @@ import { api as productActions } from '../../redux/modules/product';
 
 const SearchItem = ({image, contents, title, itemId}) => {
     const dispatch = useDispatch();
-
+    // const search_word = useSelector(state => state.search.search_word);
+   
     const onGoDetail = () => {
         dispatch(productActions.getProductApi(itemId));
     };
 
+    // const textHighlight = (search_word) => {
+    //     let regexAllCase = new RegExp(search_word, "gi");
+    //     console.log(regexAllCase, search_word)
+    //     console.log(title.replace(regexAllCase, `<span className="emp">${search_word}</span>`))
+    // };
+    // textHighlight(search_word)
     return (
         <Container onClick={onGoDetail}>
             <StyledImage src={image} />
@@ -36,6 +43,11 @@ const Container = styled.div`
     flex-direction: column;
     border-radius: 10px;
     box-shadow: 1px 2px 5px 0px rgba(0, 0, 0, 0.2);
+
+    /* span {
+        font-weight: bold;
+        font-size: 16px;
+    } */
 `;
 
 const StyledImage = styled.div`
